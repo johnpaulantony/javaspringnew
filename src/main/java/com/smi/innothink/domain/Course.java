e1c088bbed3cfd0bad9d80b5bd622063616904a7
dinesh <dineshkumarv98@gmail.com>
2019-12-16 19:24:38 +0530

    john committed

---
+++ src/main/java/com/smi/innothink/domain/Course.java
@@ -0,0 +1,50 @@
+package com.smi.innothink.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedStoredProcedureQuery;
+import javax.persistence.ParameterMode;
+import javax.persistence.StoredProcedureParameter;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "course")
+@NamedStoredProcedureQuery(name = "in_and_out_test", 
+procedureName = "sample",
+parameters = {
+   @StoredProcedureParameter(mode = ParameterMode.IN, name = "inParam", type = String.class),
+   @StoredProcedureParameter(mode = ParameterMode.IN, name = "inParam1", type = String.class),
+   @StoredProcedureParameter(mode = ParameterMode.IN, name = "outParam1", type = String.class),
+   @StoredProcedureParameter(mode = ParameterMode.OUT, name = "outParam2", type = String.class)
+})
+public class Course {
+	@Id
+	
+	private String courseID;
+	
+	private String course_name;
+	
+	private String description;
+	public String getCourseID() {
+		return courseID;
+	}
+	public void setCourseID(String courseID) {
+		this.courseID = courseID;
+	}
+	
+	public String getCourse_name() {
+		return course_name;
+	}
+	public void setCourse_name(String course_name) {
+		this.course_name = course_name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+
+}
